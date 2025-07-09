3887f26cff08047c986b749eae27b19befa7bc93
EugeneKovalskyi <tarantul.battlefield@gmail.com>
2025-07-09 18:43:47 +0300

    update docker: add nginx

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,7 +3,15 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './App.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: [
+        "http://localhost:5173",
+        "http://localhost:3001",
+        "http://comments-app.com"
+      ]
+    }
+  });
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }))
   

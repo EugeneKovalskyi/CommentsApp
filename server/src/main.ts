e2e345c02a0262419fe0cb69d9044ddec6a9050b
EugeneKovalskyi <tarantul.battlefield@gmail.com>
2025-07-14 12:00:20 +0300

    update prisma binaryTargets

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,21 +1,22 @@
-import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
-import { AppModule } from './App.module';
+import { NestFactory } from '@nestjs/core'
+import { ValidationPipe } from '@nestjs/common'
+import { AppModule } from './App.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: [
-        "http://localhost:5173",
-        "http://localhost:3001",
-        "http://45.94.156.33:80"
-      ]
-    }
-  });
+	const app = await NestFactory.create(AppModule, {
+		cors: {
+			origin: [
+				'http://localhost:5173',
+				'http://localhost:3001',
+				'http://45.94.156.33',
+				'http://45.94.156.33:80',
+			],
+		},
+	})
 
-  app.useGlobalPipes(new ValidationPipe({ transform: true }))
-  
-  await app.listen(4001);
+	app.useGlobalPipes(new ValidationPipe({ transform: true }))
+
+	await app.listen(4001)
 }
 
-bootstrap();
+bootstrap()
